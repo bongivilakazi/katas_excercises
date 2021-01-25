8ac66b7ac1b500a028e6abe8ef5b7748f1d96216
bongivilakazi <57225184+bongivilakazi@users.noreply.github.com>
2021-01-25 10:40:33 +0200

    Update iscoceles.js

--- iscoceles.js
+++ iscoceles.js
@@ -1,18 +1,14 @@
-//create a function 
+
  function iscoceles(number) {
   for (var i = 0; i < number; i++) {
-    var str = '';// create an empty variable
+    var str = '';
     for (var j = 1; j < number-i; j++) {
-      str = str + ' ';//assign the variable string to the var string variable
-    }
+      str = str + ' ';
     for (var k = 1; k <= (2*i+1); k++) {
       str = str + '*'
     }
-  
-    //multiply i by 2 and add 1
-      //place a forloop to loop through 
     console.log(str);
   }
 }
 
-iscoceles(8);//pass in any number to print a pyramid
+iscoceles(8);
