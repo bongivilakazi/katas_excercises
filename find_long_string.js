f1e0521d414d543ceabc6db167f282050c2d8d16
bongivilakazi <57225184+bongivilakazi@users.noreply.github.com>
2021-01-25 10:38:41 +0200

    Update find_long_string.js

--- find_long_string.js
+++ find_long_string.js
@@ -1,11 +1,11 @@
 var pattern = ["chill","relax","eaten"];
-var outstretchedWord = function(pattern){
+var outStretchedWord = function(pattern){
     var longestTerm = pattern.reduce(function(a,b){
     return (a.length > b.length) ? a : b;
   });
     return longestTerm;
 }
-console.log(outstretchedWord(pattern));
+console.log(outStretchedWord(pattern));
 
 
 
