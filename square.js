622f6ee26e32c49d17dd827ff9ce7303f6551779
bongivilakazi <57225184+bongivilakazi@users.noreply.github.com>
2021-01-25 10:39:51 +0200

    Update square.js

--- square.js
+++ square.js
@@ -1,19 +1,13 @@
 
 
-function square(hashtags)//create a function called hasthtags
+function square(hashtags)
 {
- for(i=1; i<=hashtags; i++)/*create a forloop that starts at 1 
-                            and loops whileit is less 
-                            than or equal to the number passed in*/                           
+ for(i=1; i<=hashtags; i++)                           
  {
-  for(j=1; j<=hashtags; j++)/*create another forloop that starts at 1 
-  and loops  and increments while it is less 
-  than or equal to the number passed in and increments*/                           
-{
-   console.log('#')//log out the hashtag
+  for(j=1; j<=hashtags; j++)
+   console.log('#')
 }
-   console.log('<br/>')//place a <br> tag to break after each print
-}
+   console.log('<br/>')
 }
 square(5)
 
