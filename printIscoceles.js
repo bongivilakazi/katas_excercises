3bef78595123468659c63f15a74a501c23df4177
bongivilakazi <57225184+bongivilakazi@users.noreply.github.com>
2021-01-28 07:07:27 +0200

    Update printIscoceles.js

--- printIscoceles.js
+++ printIscoceles.js
@@ -1,12 +1,14 @@
 
- function iscoceles(number) {
+function iscoceles(number) {
   for (var i = 0; i < number; i++) {
-    var str = '';
-    for (var j = 1; j < number-i; j++) {
-      str = str + ' ';
-    for (var k = 1; k <= (2*i+1); k++) {
-      str = str + '*'
+    var str = "";
+    for (var j = 1; j < number - i; j++) {
+      str = str + " ";
     }
+    for (var k = 1; k <= 2 * i + 1; k++) {
+      str = str + "*";
+    }
+
     console.log(str);
   }
 }
