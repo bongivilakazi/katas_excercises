ee8b6bb2f1b8f7c8a06354a17c98e43c6c036a21
bongivilakazi <57225184+bongivilakazi@users.noreply.github.com>
2021-02-18 04:05:06 +0200

    Update printSquare.js

--- printSquare.js
+++ printSquare.js
@@ -1,13 +1,9 @@
 function printSquare(digit){
-    var pattern = "";
-    var k, j;
-    for (j = 0; j < digit; j++){
-        for (k = 0; k < digit; k++){
-            pattern = pattern + "#";
-        }
+   var pattern;
+    for(let i = 0; i < digit; i++){
+        pattern = '#'.repeat(digit);
         console.log(pattern);
-        pattern = "";
-    } 
+    }
 }
 printSquare(4);
 
