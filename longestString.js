bd27587d3143b213afaf2728acf4946873148e75
bongivilakazi <57225184+bongivilakazi@users.noreply.github.com>
2021-02-18 04:17:15 +0200

    Update longestString.js

--- longestString.js
+++ longestString.js
@@ -1,14 +1,19 @@
-var pattern = ["chill","relax","eaten"];
-var outStretchedWord = function(pattern){
-    var longestWord = pattern.reduce(function(a,b){
-    return (a.length > b.length) ? a : b;
-  });
-    return longestTerm;
+
+function longestString(arr){
+    var number;
+    let emptyArray= [];
+    for(let i = 0; i < arr.length; i++){
+        number = arr[i].length
+       emptyArray.push(number)
+    } 
+    let lengthChecker = Math.max(...emptyArray)
+    for(let j = 0; j < arr.length; j++){
+        if(lengthChecker === arr[j].length){
+            console.log(arr[j])
+        } 
+    }
 }
-console.log(outStretchedWord(pattern));
-
-
-
+longestString(["oop", "React", "learn"', "eat"])
 
 
 
