ff520f2a5b24cbdc44267daf73ada357381fc670
bongivilakazi <57225184+bongivilakazi@users.noreply.github.com>
2021-02-19 06:06:59 +0200

    Update joiningArrays.js

--- joiningArrays.js
+++ joiningArrays.js
@@ -1,13 +1,22 @@
-function combinedArray(arr1, arr2) {
-  if (arr1.length == arr2.length) {
-    var arrayCombination = [];
-    for (var i = 0; i < arr1.length; i++) {
-      arrayCombination.push(arr1[i], arr2[i]);
+function joinArrays(arr1,arr2){
+    let firstArray = [];
+    let secondArray = [];
+    for(let i = 0; i < arr1.length ||i < arr2.length; i++){
+        firstArray = [arr1[i],arr2[i]];
+        secondArray.push(arr1);
     }
-    console.log(arrayCombination);
-  } else {
-    console.log(arrayCombination);
-  }
+    let str = secondArray.join(','); 
+    let thirdArray = str.split(',');
+
+    let combinedArray;
+    let mergedArray = [];
+    for(let j = 0; j < thirdArray.length; j++){
+        combinedArray = parseInt(thirdArray[j]);
+        newArray.push(combinedArray);
+
+    }
+    console.log(newArray);         
+    
 }
-combinedArray([11, 22, 33], [1, 2, 3]);
+joinArrays([11,22,33], [1,2,3]);
 
