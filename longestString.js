1e3f86704dfde60cd20936fd0a788d33bea6df97
bongivilakazi <57225184+bongivilakazi@users.noreply.github.com>
2021-02-21 21:50:14 +0200

    Update longestString.js

--- longestString.js
+++ longestString.js
@@ -14,6 +14,22 @@ function longestString(arr){
     }
 }
 longestString(["oop", "React", "learn"', "eat"]);
+               
+     function longest(arr){
+    let count
+    let newArr = []
+    for(let i = 0; i < arr.length; i++){
+        count = arr[i].length
+        newArr.push(count)
+    } 
+    let checkMaxNum = Math.max(...newArr)
+    for(let j = 0; j < arr.length; j++){
+        if(checkMaxNum === arr[j].length){
+            console.log(arr[j])
+        } 
+    }
+}
+longest(['once', 'chicken', 'a', 'time'])
 
 
 
