c429cc8c5289e431d1df42098503f4756d0352a6
bongivilakazi <57225184+bongivilakazi@users.noreply.github.com>
2021-02-21 20:53:22 +0200

    Update joiningArrays.js

--- joiningArrays.js
+++ joiningArrays.js
@@ -1,14 +1,14 @@
 function combineTwoArrays(arr1, arr2) {
-	var combinedArray = [];
+	var joinedArray = [];
 
 	for (var i = 0; i < arr1.length; i++) {
-		combinedArray.push(arr1[i]);
+		joinedArray.push(arr1[i]);
 	}
 	for (var j = 0; j < arr2.length; j++) {
-		combinedArray.push(arr2[j]);
+		joinedArray.push(arr2[j]);
 	}
 
-	return combinedArray.sort();
+	return .sort();
 }
 
 console.log(combineTwoArrays([11, 22, 33], [1, 2, 3]))
